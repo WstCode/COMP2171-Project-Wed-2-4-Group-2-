cb68a4b470e499cf8822c545ba48f2aeff0fef4d
WstCode <laeltbrown@gmail.com>
2026-03-10 12:12:38 -0500

    Create OrderItem class with properties and methods

---
+++ OrderItem.java
@@ -0,0 +1,40 @@
+public class OrderItem {
+    private String itemName;
+    private int quantity;
+    private double unitPrice;
+
+    public OrderItem(String itemName, int quantity, double unitPrice) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getSubTotal() {
+        return quantity * unitPrice;
+    }
+
+}
