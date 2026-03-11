0a72fcc5dbcd7cc7e58729c59a08f74fbd3c32d2
bdeyonce6-sudo <bdeyonce6@gmail.com>
2026-03-10 21:59:26 -0500

    Add files via upload

--- OrderManager.java
+++ OrderManager.java
@@ -55,3 +55,24 @@ public class OrderManager {
     }
 }
 
+public class Main {
+    public static void main(String[] args) {
+        BakeryStaff staff = new BakeryStaff("S001", "admin", "pass123");
+        OrderManager manager = new OrderManager("orders.txt", "customers.txt");
+
+        if (!staff.login("admin", "pass123")) {
+            System.out.println("Login failed.");
+            return;
+        }
+
+        Customer customer = new Customer("C001", "Janice Brown", "8761234567", "Spanish Town");
+        staff.createCustomer(customer);
+
+        List<OrderItem> items = new ArrayList<>();
+        items.add(new OrderItem("Birthday Cake", 1, 4500.00));
+        items.add(new OrderItem("Cupcakes", 12, 250.00));
+
+        staff.createOrder(manager, customer, items, LocalDate.of(2026, 3, 20));
+    }
+}
+
